215bc2f277e2ead2284632b50e099d1a5735975d
yasoug <ougouisyassine@gmail.com>
2023-10-22 06:38:13 -0700

    task 3 : commit 1

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""This script starts Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello():
+    """/: displays (Hello HBNB!)"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    """/hbnb: displays (HBNB)"""
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def C_text(text):
+    """/c/<text>: displays C followed by the text variable"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python')
+@app.route('/python/<text>')
+def python_text(text="is cool"):
+    """
+    /python/<text>: displays Python followed by the text variable
+    """
+    return "Python {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
