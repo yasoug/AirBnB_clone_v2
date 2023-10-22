037a69d2afb5df5afa4689aa0032359bcd3dae63
yasoug <ougouisyassine@gmail.com>
2023-10-22 08:33:46 -0700

    task 8 : commit 2

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,18 +10,14 @@ app.url_map.strict_slashes = False
 
 
 @app.route("/states_list")
-def states_list():
-    """
-    /states_list: displays html page
-    H1 tag: “States”
-    UL tag: with the list of all State objects present in DBStorage
-    """
-    states = [s for s in storage.all("State").values()]
+def states():
+    """displays html page"""
+    states = storage.all(State)
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
-def tear_down(self):
+def teardown(exception):
     """Remove current SQLAlchemy session, after each request"""
     storage.close()
 
